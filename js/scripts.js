c3a92e3dafde9d4e5b494f4430608f62eab29a2e
jawittdesigns <Killjoy11>
2016-02-17 06:42:46 -0500

    Fixed accordian triger

--- js/scripts.js
+++ js/scripts.js
@@ -2112,9 +2112,9 @@ $(document).ready(function() {
   'use strict';
 
   $('.projects__heading').append('<div class="accordian closed"></div>');
-  $('.projects__heading').on( 'click', function() {
-    $(this).find('.accordian').toggleClass('opened');
-    $(this).next('ul').slideToggle();
+  $('.projects__heading .accordian').on( 'click', function() {
+    $(this).toggleClass('opened');
+    $(this).parent().next('ul').slideToggle();
   })
 
 })( jQuery );
